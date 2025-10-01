3073fc79764c56c2e35cb12a536f3f5793bef85f
Lucasfeva <luscas.png@gmail.com>
2025-10-01 14:42:05 -0300

    chore: add `three.js` and `3d-tiles-renderer`

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  transpilePackages: ['three', '3d-tiles-renderer'],
 };
 
 export default nextConfig;
